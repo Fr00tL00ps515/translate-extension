a5a8a751d840787d494586bf69fc4ed59e7abf78
Vladyslav <vlad0902v@gmail.com>
2026-08-21 14:53:44 +0200

    created mvp

--- server/main.py
+++ server/main.py
@@ -1,27 +1,62 @@
 from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Word():
+    def __init__(self, english, russian):
+        self.english = english
+        self.russian = russian
+
+    def get(self):
+        return {self.english : self.russian}
+    def getEnglish(self):
+        return self.english
+
+    
 class DB():
     def __init__(self):
-        self.words = []
+        self.words : list[Word] = []
 
     def add(self, english : str, russian : str) -> bool:
         for i in self.words:
-            if i['english'] == english:
+            if i.getEnglish() == english:
                 return False 
-        self.words.append({'english' : english, 'russian' : russian})
+        self.words.append(Word(english, russian))
+        
         return True
+
+    def getAll(self):
+        res : dict = {}
+        for i in self.words:
+            res.update(i.get())
+        return res
 
 
 db = DB()
 
 @app.post('/')
 def addNewWord(english : str, russian : str, response : Response):
+    print('try to add a new word: ' + english)
     res : bool = db.add(english, russian)
     if res: 
         response.status_code = status.HTTP_400_BAD_REQUEST
     response.status_code = status.HTTP_200_OK
     return {} 
     
+
+
+@app.get('/')
+def getAllWords():
+    return db.getAll()
